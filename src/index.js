fe4597d2f5bbe1d6f7b724403459704a4b4b3f91
jadx2 <delmarjaim@gmail.com>
2021-05-12 15:10:29 -0500

    Delete commented code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import Navbar from './navbar';
 import Footer from './footer';
